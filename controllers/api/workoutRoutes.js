2402ea1f5965f8aff932e8fa408a4e57d9b9c858
Chito <Javier.martinez632@gmail.com>
2023-01-05 20:01:56 -0700

    updated get routes

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,34 +1,36 @@
 const router = require('express').Router();
-const { Workout_category } = require('../../models');
+const { Workout } = require('../../models');
 
 router.post('/', async (req, res) => {
     try {
-        const newWorkoutCategory = await Workout_category.create({
+        const workoutData = await Workout.create({
             ...req.body,
             user_id: req.session.user_id,
         });
 
-        res.status(200).json(newWorkoutCategory);
+        res.status(200).json(workoutData);
     } catch (err) {
         res.status(400).json(err);
     }
 });
 
+
+
 router.delete('/:id', async (req, res) => {
     try {
-        const workCateData = await Workout_category.destroy({
+        const workoutData = await Workout.destroy({
             where: {
                 id: req.params.id,
                 user_id: req.session.user_id,
             },
         });
 
-    if(!workCateData) {
-        res.status(404).json({ message: 'No Category found with that id!' });
+    if(!workoutData) {
+        res.status(404).json({ message: 'No Workout found with that id!' });
         return;
     }
 
-    res.status(200).json(workCateData);
+    res.status(200).json(workoutData);
     } catch (err) {
       res.status(500).json(err);  
     }
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,47 @@
 const router = require('express').Router();
-const { User, Workout_category, Goal } = require('../models');
+const { User, Goal, Workout } = require('../models');
 //const withAuth = require('../utils/auth');
 
+
+
 router.get('/', async (req, res) => {
+  try {
+    res.render('homepage', {
+      //logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', async (req, res) => {
+  try {
+    if(req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+  res.render('login');
+});
+
+
+
+// router.get('/main', async (req, res) => {
+//   try {
+
+//     res.render('main', {
+//      //goals,
+//       logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+router.get('/goals', async (req, res) => {
   try {
     const goalData = await Goal.findAll({
         include: [
@@ -15,7 +54,7 @@ router.get('/', async (req, res) => {
     
     const goals = goalData.map((goal) => goal.get({ plain: true }));
 
-    res.render('homepage', {
+    res.render('goals', {
       goals,
       logged_in: req.session.logged_in
     });
@@ -24,27 +63,27 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/goal/:id', async (req, res) => {
+router.get('/workouts', async (req, res) => {
   try {
-    const goalData = await Goal.findByPk(req.params.id, {
-      include: [
-        {
-          model:User,
-          attributes: ['username'],
-        },
-      ],
-    });
-
-    const goal = goalData.get({ plain:true });
-
-    res.render('goal', {
-      ...goal,
+      const workoutData = await Workout.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['username'],
+          },
+        ],
+      });
+    
+      const workouts = workoutData.map((goal) => goal.get({ plain: true }));
+    res.render('workouts', {
       logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+
 
 //add middleware here 
 
