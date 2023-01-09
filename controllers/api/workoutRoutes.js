9333ae0a5334594a51f799d41181910bea10765d
FruityOkapi <josephbsimmerman@gmail.com>
2023-01-08 23:56:23 -0500

    fixed typos in frontend and added user_id to routes

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -14,7 +14,10 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const workoutData = await Workout.create(req.body);
+        const workoutData = await Workout.create({
+          ...req.body,
+        user_id: req.session.user_id
+      });
         res.status(200).json(workoutData);
     } catch (err) {
         res.status(400).json(err);
